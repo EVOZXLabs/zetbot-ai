963c3c6cb2a91af162ff8c84fa1703b8e8304bdc
EVOZXLabs <agustriyadiofficial@gmail.com>
2026-07-03 21:34:49 +0700

    Implement Exchange module for trading with ccxt

---
+++ bot/exchange.py
@@ -0,0 +1,64 @@
+"""
+====================================
+
+Exchange Module
+
+ZetBot AI
+
+====================================
+"""
+
+import ccxt
+
+from config import CONFIG
+
+
+class Exchange:
+
+    def __init__(self):
+
+        exchange_name = CONFIG["exchange"].lower()
+
+        if exchange_name == "binance":
+
+            self.exchange = ccxt.binance({
+                "apiKey": CONFIG["api_key"],
+                "secret": CONFIG["api_secret"],
+                "enableRateLimit": True
+            })
+
+        elif exchange_name == "bybit":
+
+            self.exchange = ccxt.bybit({
+                "apiKey": CONFIG["api_key"],
+                "secret": CONFIG["api_secret"],
+                "enableRateLimit": True
+            })
+
+        elif exchange_name == "tokocrypto":
+
+            self.exchange = ccxt.binance({
+                "apiKey": CONFIG["api_key"],
+                "secret": CONFIG["api_secret"],
+                "enableRateLimit": True
+            })
+
+        else:
+
+            raise Exception("Exchange tidak didukung.")
+
+    def ticker(self):
+
+        return self.exchange.fetch_ticker(
+            CONFIG["symbol"]
+        )
+
+    def last_price(self):
+
+        ticker = self.ticker()
+
+        return ticker["last"]
+
+    def balance(self):
+
+        return self.exchange.fetch_balance()
